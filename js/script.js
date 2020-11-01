8b3bbc07c3cb17995cbe264c651941947a97bad1
Kateryna <k.khalimovska@gmail.com>
2020-11-01 20:16:16 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+ let neme = prompt ("Как вас зовут?");
+ let age = prompt ("Сколько вам лет?");
+
+ if (age<16){
+     console.log(`Привет ${neme}! Вы подросток!`)
+ } else if (age>16 && age<40){
+    console.log(`Привіт ${neme}! Вы молодой человек!`)
+} else if (age>40){
+    console.log(`Привіт ${neme}! Вы старичок!`)
+}
+
+
+
