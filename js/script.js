6ddba60f0ac6da200335f4dfe03eef6badd97648
Kateryna <k.khalimovska@gmail.com>
2020-11-02 21:29:15 +0200

    Робота над повідомленням -помилка

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
  let neme = prompt ("Как вас зовут?");
  let age = prompt ("Сколько вам лет?");
 
- if (age<16){
+ if (age==0){
+    alert(`Ошибка`)
+ } else if (age<16){
      console.log(`Привет ${neme}! Вы подросток!`)
  } else if (age>16 && age<40){
     console.log(`Привeт ${neme}! Вы молодой человек!`)
 } else if (age>40){
     console.log(`Привeт ${neme}! Вы старичок!`)
-} else if (age==0){
-   alert(`Ошибка`)
 }
-
-
-
