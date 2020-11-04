47d895eacba674ac2ecc44c1945da51575f5b9a7
Kateryna <k.khalimovska@gmail.com>
2020-11-04 19:14:42 +0200

    Нова версія файлу js

--- lesson_js_2_Kateryna/js/script.js
+++ lesson_js_2_Kateryna/js/script.js
@@ -1,56 +1,19 @@
 
-// let fruitsEng = ["Plum", "Peach", "Pineapple", "Cherry", "Pear", "Apple"];
+    let btnEng = document.querySelector(".button-eng");
+    let btnRus = document.querySelector(".button-rus");
 
-// for (let i = 0; i < fruitsEng.length; i++) {
-//   console.log(fruitsEng [i]);  
-// }
-
-// let btnEng = document.querySelector(".button-eng");
-
-// alert.log(btnEng)
-
-// btnEng.addEventListener("click", fruitsEng)
-
-
-
-// let fruitsRus = ["Слива", "Персик", "Ананас", "Вишня", "Груша", "Яблоко"];
-// for (let i = 0; i < fruitsRus.length; i++) {
-//     console.log(fruitsRus[i]); 
-// }
-
-// let btnRus = document.querySelector(".button-rus");
-
-// .log(btnRus)
-
-// btnRus.addEventListener("click", fruitsRus)
-
-
-
-
-
-// Многомерний масив
-let arr = {
-
+    let arr = {
     "fruitsEng":["Plum", "Peach", "Pineapple", "Cherry", "Pear", "Apple"],
-    
     "fruitsRus": ["Слива", "Персик", "Ананас", "Вишня", "Груша", "Яблоко"],
 }
-let btnEng = document.querySelector(".button-eng");
-
-console.log(btnEng);
 
 btnEng.addEventListener("click", function (){
-    alert(arr(fruitsEng))
+    arr["fruitsEng"].forEach(function(elem){
+        console.log(elem)
+    })
 });
-
-
-
-
-// for (let i = 0; i < fruits_eng.length; i++) {
-//     console.log(arr["fruits_eng"][i]);
-  
-//     }
-    
-// for (let i = 0; i < fruits_eng.length; i++) {
-//     console.log(fruits_rus[i]); 
-//     }
+btnRus.addEventListener("click", function (){
+    arr["fruitsRus"].forEach(function(elem){
+        console.log(elem)
+    })
+});
