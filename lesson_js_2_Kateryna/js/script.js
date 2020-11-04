a2b822bb8bf4e22ca0af143895ea7445004b695c
Kateryna <k.khalimovska@gmail.com>
2020-11-04 17:26:57 +0200

    Оновлення папки

---
+++ lesson_js_2_Kateryna/js/script.js
@@ -0,0 +1,48 @@
+
+let fruitsEng = ["Plum", "Peach", "Pineapple", "Cherry", "Pear", "Apple"];
+
+for (let i = 0; i < fruitsEng.length; i++) {
+  console.log(fruitsEng [i]);  
+}
+
+let btnEng = document.querySelector(".button-eng");
+
+alert.log(btnEng)
+
+btnEng.addEventListener("click", fruitsEng)
+
+
+
+let fruitsRus = ["Слива", "Персик", "Ананас", "Вишня", "Груша", "Яблоко"];
+for (let i = 0; i < fruitsRus.length; i++) {
+    console.log(fruitsRus[i]); 
+}
+
+let btnRus = document.querySelector(".button-rus");
+
+alert.log(btnRus)
+
+btnRus.addEventListener("click", fruitsRus)
+
+
+
+
+
+// Многомерний масив
+// let arr = {
+
+//     "fruits_eng":["Plum", "Peach", "Pineapple", "Cherry", "Pear", "Apple"],
+    
+//     "fruits_rus": ["Слива", "Персик", "Ананас", "Вишня", "Груша", "Яблоко"],
+// }
+
+
+
+// for (let i = 0; i < fruits_eng.length; i++) {
+//     console.log(arr["fruits_eng"][i]);
+  
+//     }
+    
+// for (let i = 0; i < fruits_eng.length; i++) {
+//     console.log(fruits_rus[i]); 
+//     }
